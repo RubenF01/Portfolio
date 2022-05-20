1f5cf746b06bef3437fbfb94ebafcdbb526969bc
RubenF01 <51177012+RubenF01@users.noreply.github.com>
2022-05-20 11:04:20 -0400

    Fixed project state and added GitHub links

--- components/Project.js
+++ components/Project.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import Link from "next/dist/client/link";
 import { motion } from "framer-motion";
 
-const Project = ({ name, image, technologies, link }) => {
+const Project = ({ name, image, technologies, link, linkGithub }) => {
   const [isShown, setIsShown] = useState(false);
 
   return (
     <div
-      onMouseEnter={() => setIsShown(!isShown)}
-      onMouseLeave={() => setIsShown(!isShown)}
-      className={`relative`}
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+      className="relative"
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
@@ -27,15 +27,27 @@ const Project = ({ name, image, technologies, link }) => {
       >
         <h1 className="text-2xl 2xl:text-4xl">{name}</h1>
         <p className="mb-5 2xl:text-lg">{technologies}</p>
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 1.1 }}
-          className="border-2 rounded px-3 py-1"
-        >
-          <Link href={link}>
-            <a target="_blank">VISIT SITE</a>
-          </Link>
-        </motion.button>
+        <div className="flex space-x-4">
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
+            className="border-2 rounded px-3 py-1"
+          >
+            <Link href={linkGithub}>
+              <a target="_blank">GitHub</a>
+            </Link>
+          </motion.button>
+
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1.1 }}
+            className="border-2 rounded px-3 py-1"
+          >
+            <Link href={link}>
+              <a target="_blank">VISIT SITE</a>
+            </Link>
+          </motion.button>
+        </div>
       </div>
     </div>
   );
